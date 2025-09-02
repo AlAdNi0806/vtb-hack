bbfc11782873d7133d46928436a8eac7662505a4
AlAdNi0806 <alschr0806@gmail.com>
2025-09-02 15:12:32 +0300

    change of model

--- backend/model.py
+++ backend/model.py
@@ -1,7 +1,7 @@
 import nemo.collections.asr as nemo_asr
 
 # Load a pretrained ASR model (English example, replace with Russian model if needed)
-asr_model = nemo_asr.models.ASRModel.from_pretrained("nvidia/stt_en_fastconformer_transducer_large")
+asr_model = nemo_asr.models.ASRModel.from_pretrained("nvidia/stt_ru_fastconformer_hybrid_large_pc")
 
 # Path to your audio file (wav, 16 kHz recommended)
 audio_file = "./examples_english_english.wav"
@@ -11,3 +11,9 @@ transcripts = asr_model.transcribe([audio_file])
 
 print("Transcription:")
 print(transcripts[0])
+
+
+# models:
+# nvidia/stt_en_fastconformer_transducer_large
+# nvidia/stt_ru_fastconformer_hybrid_large_pc - with spaces periods commas and other
+# nvidia/stt_ru_conformer_transducer_large
